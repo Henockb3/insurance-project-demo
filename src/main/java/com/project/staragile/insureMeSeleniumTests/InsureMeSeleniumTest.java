ace4448cadd8ac4e587b9de213e28c195adf9ee9
henockb3 <henockb3@gmail.com>
2025-03-02 00:49:20 -0800

    selenium update

--- src/main/java/com/project/staragile/insureMeSeleniumTests/InsureMeSeleniumTest.java
+++ src/main/java/com/project/staragile/insureMeSeleniumTests/InsureMeSeleniumTest.java
@@ -1,22 +1,32 @@
 package com.project.staragile.insureMeSeleniumTests;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+public class InsureMeSeleniumTest {
 
-public class InsureMeSeleniumTest{
-    
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
-        WebDriver driver = new ChromeDriver();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless");
-		try {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-software-rasterizer");
+        options.addArguments("--disable-logging");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        try {
             driver.get("http://18.218.238.178:8081/contact.html");
-			System.out.println(driver.getTitle());
+            System.out.println("Page Title: " + driver.getTitle());
+
             WebElement name = driver.findElement(By.id("inputName"));
             name.sendKeys("Test Name");
 
@@ -30,17 +40,17 @@ public class InsureMeSeleniumTest{
             message.sendKeys("Hello, this is a test message.");
 
             WebElement submitButton = driver.findElement(By.id("my-button"));
-			submitButton.click();
+            submitButton.click();
 
             WebElement responseMessage = driver.findElement(By.id("response"));
-			responseMessage.getText();
-			if(responseMessage.getText().equalsIgnoreCase(("Message Sent"))){
-				System.out.println("Test Case Passed");
-			}else{
-				System.out.println("Test Case Failed");
-			}
-			
+            String responseText = responseMessage.getText();
+            System.out.println("Response Message: " + responseText);
 
+            if (responseText.equalsIgnoreCase("Message Sent")) {
+                System.out.println("Test Case Passed");
+            } else {
+                System.out.println("Test Case Failed");
+            }
 
         } finally {
             driver.quit();
