9f4fee64318219316aa6a7fa94d62f2efd4de869
Henockb3 <52932865+Henockb3@users.noreply.github.com>
2025-03-02 03:13:40 -0800

    Update InsureMeSeleniumTest.java

--- src/main/java/com/project/staragile/insureMeSeleniumTests/InsureMeSeleniumTest.java
+++ src/main/java/com/project/staragile/insureMeSeleniumTests/InsureMeSeleniumTest.java
@@ -12,7 +12,7 @@ public class InsureMeSeleniumTest {
         System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
 
         ChromeOptions options = new ChromeOptions();
-        options.setBinary("/opt/google/chrome/chrome-linux64/chrome");      
+        options.setBinary("/usr/bin/google-chrome");    
         options.addArguments("--headless");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
